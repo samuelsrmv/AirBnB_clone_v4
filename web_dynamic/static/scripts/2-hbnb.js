ea77bb0d33ea0b433d39b341b8d044f3bc49cad3
samuelsrmv <samuel.srmv@gmail.com>
2021-05-16 10:01:39 -0500

    fix 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,32 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  console.log('Se ejecuta 1');
 
-    console.log("Se ejecuta 1");
+  const amenitiesList = [];
 
-    let amenitiesList = [];
+  $('input[type=checkbox]').change(function () {
+    console.log('check');
+    const dataId = $(this).attr('data-name');
+    console.log(dataId);
+    if ($(this).is(':checked')) {
+      amenitiesList.push(dataId);
+    } else {
+      const myIndex = amenitiesList.indexOf(dataId);
+      amenitiesList.splice(myIndex, 1);
+    }
 
-    $("input[type=checkbox]").change(function() {
-
-        console.log("check")
-        let dataId = $(this).attr("data-name");
-        console.log(dataId);
-        if ($(this).is(":checked")) {
-            amenitiesList.push(dataId);
-        }
-        else {
-            let myIndex = amenitiesList.indexOf(dataId);
-            amenitiesList.splice(myIndex, 1);
-        }
-
-        $(".amenities > h4").text(amenitiesList);
-    });
-
-
-    $.get("http://0.0.0.0:5001/api/v1/status/", function(data){
-        console.log(data, data.body, data.status);
-        if (data === 200) {
-            $("#api_status").addClass("available");
-        } else {
-            $("#api_status").removeClass("available");
-        }
-    });
+    $('.amenities > h4').text(amenitiesList);
+  });
+});
+const url = 'http://0.0.0.0:5001/api/v1/status/';
+$.getJSON(url, function (data) {
+  console.log('si entro', data, data.status);
+  if (data.status === 'OK') {
+    $('#api_status').addClass('available');
+  } else {
+    $('#api_status').removeClass('available');
+  }
 });
